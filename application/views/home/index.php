afc4703d4aa45c68816524e9d8000fd524d376dd
Adhitya Sidiq Permana <adhityasp18@gmail.com>
2019-11-29 01:55:24 +0700

    Initial Commit & Creating Home View

---
+++ application/views/home/index.php
@@ -0,0 +1,19 @@
+<div class="container">
+  <div class="row mt-5">
+    <div class="col-md-5">
+      <a class="text-decoration-none text-body" href="">
+        <div class="card" style="width: 18rem;">
+          <img src="<?= base_url(); ?>assets/download.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+              <hr>
+              <h5>Nama Barang</h5>
+              <hr>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content</p>
+              <hr>
+              <h6>Rp.000.00</h6>
+          </div>
+        </div>
+      </a>
+    </div>
+  </div>
+</div>
