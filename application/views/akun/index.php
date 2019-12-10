860e4a431f75fe4e68351abd257d8d065664df9e
azharhamdani8 <azhar.153040054@mail.unpas.ac.id>
2019-12-10 17:35:43 +0700

    menambahkan controller akun dan view akun

--- application/views/akun/index.php
+++ application/views/akun/index.php
@@ -1,5 +1,5 @@
-<div class="row">
-  <div class="col-md-3">
+<div class="row my-3">
+  <div class="col-2">
     <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical" >
       <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Profile saya</a>
     
@@ -12,20 +12,24 @@
       <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
         
       <div class="container">
-          <div class="row mt-3">
-              <div class="col-md-6">
+          <div class="row mt-4">
+              <div class="col-md-8">
                   <div class="card">
                       <div class="card-header">
                        Profile saya
                       </div>
-                      <div class="card-body">
-                          <h5 class="card-title">nama</h5>
-                          <h6 class="card-subtitle mb-2 text-muted">alamat</h6>
-                          <!-- <p class="card-text"><?= $mahasiswa['nrp']; ?></p>
-                          <p class="card-text"><?= $mahasiswa['jurusan']; ?></p> -->
-                          <a href="" class="btn btn-primary">Ubah Profile</a>
+                      <ul class="list-group">
+                      <li class="list-group-item">
+                      <div class="row">
+                      <h4>Nama :</h4><p> Cras justo odio</p>
                       </div>
-                  </div>
+                      </li>
+                      <li class="list-group-item">Dapibus ac facilisis in</li>
+                      <li class="list-group-item">Morbi leo risus</li>
+                      <li class="list-group-item">Porta ac consectetur ac</li>
+                      <li class="list-group-item">Vestibulum at eros</li>
+                    </ul>
+                                      </div>
               </div>
           </div>
 
