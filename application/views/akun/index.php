aee6549b6c7b800e88456515dab6475183b77031
azharhamdani8 <azhar.153040054@mail.unpas.ac.id>
2019-12-10 17:02:09 +0700

    menambahkan controller akun dan view akun

---
+++ application/views/akun/index.php
@@ -0,0 +1,41 @@
+<div class="row">
+  <div class="col-md-3">
+    <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical" >
+      <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Profile saya</a>
+    
+      <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Kebijakan privasi</a>
+      
+    </div>
+  </div>
+  <div class="col-7">
+    <div class="tab-content" id="v-pills-tabContent">
+      <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
+        
+      <div class="container">
+          <div class="row mt-3">
+              <div class="col-md-6">
+                  <div class="card">
+                      <div class="card-header">
+                       Profile saya
+                      </div>
+                      <div class="card-body">
+                          <h5 class="card-title">nama</h5>
+                          <h6 class="card-subtitle mb-2 text-muted">alamat</h6>
+                          <!-- <p class="card-text"><?= $mahasiswa['nrp']; ?></p>
+                          <p class="card-text"><?= $mahasiswa['jurusan']; ?></p> -->
+                          <a href="" class="btn btn-primary">Ubah Profile</a>
+                      </div>
+                  </div>
+              </div>
+          </div>
+
+      </div>
+
+
+
+      </div>
+      <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">...</div>
+      <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">...</div>
+    </div>
+  </div>
+</div>
