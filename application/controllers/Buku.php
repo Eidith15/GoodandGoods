a7e22a9c249978af6fc0b4a7f2b7359829751732
nurulfauziahh <nurul_173040092@mail.unpas.ac.id>
2019-12-17 19:24:49 +0700

    memindahkan controller untuk detail

---
+++ application/controllers/Buku.php
@@ -0,0 +1,21 @@
+<?php 
+class Buku extends CI_Controller {
+
+	public function __construct(){
+		parent::__construct();
+		$this->load->library('cart');
+		$this->load->model('Home_model');
+		// $this->load->model('MKeranjang');
+	}
+
+	public function detail($id){
+		$data['judul'] = 'Detail Buku';
+		$kategori=($this->uri->segment(3))?$this->uri->segment(3):0;
+		$data['barang'] = $this->Home_model->get_produk_kategori($kategori);
+		$data['kategori'] = $this->Home_model->get_kategori_all();
+		$data['barang'] = $this->Home_model->getBarangById($id);
+		$this->load->view('templates/header', $data);
+		$this->load->view('home/detail', $data);
+		$this->load->view('templates/footer');
+	}
+}
